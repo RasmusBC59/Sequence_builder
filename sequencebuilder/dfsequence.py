a1d02b9838bc265560e811fbe2135c7861a30d5f
Rasmus B. Christensen <rasmus.christensen@nbi.ku.dk>
2022-02-22 17:36:28 +0100

    change order of functions

--- sequencebuilder/dfsequence.py
+++ sequencebuilder/dfsequence.py
@@ -6,31 +6,59 @@ import numpy as np
 ramp = bb.PulseAtoms.ramp
 
 
-def strtolist(s):
-    values = [float(i) for i in s.split(',')]
-    if len(values) == 3:
-        values[-1] = int(values[-1])
-    return values
+def df_to_seq(df):
+    seq = bb.Sequence()
+    nr_elem = find_recurcive(df)
+    chan_list = find_channels(df)
+    id = df.index.values
+    idnr = id.shape[0]
+    for h in range(nr_elem):
+        elem = bb.Element()
+        for ch in chan_list:
+            chnr = int(ch[2:])
+            bp = bb.BluePrint()
+            bp.setSR(1.2e9)
+            for i in range(idnr):
+                nm = df.iloc[i][['name']].values[0]
+                volt = df.iloc[i][[ch]].values[0]
+                volt = getvoltvalues(volt, h)
+                dr = df.iloc[i][['time']].values[0]
+                dr = get_time(dr, h)*1e-6
+                bp.insertSegment(i, ramp, volt, name=nm, dur=dr)
+                add_marker(bp, df, i, chnr, 1, nm, dr)
+                add_marker(bp, df, i, chnr, 2, nm, dr)
+            elem.addBluePrint(chnr, bp)
+        seq.addElement(h+1, elem)
+    seq.setSR(1.2e9)
+    return seq
 
 
-def list_or_sting(val):
-    if type(val) == str:
-        return strtolist(val)
-    else:
-        return val
+def add_marker(bp, df, i, chnr, mnr, nm, dr):
+    if df.iloc[i][[f'm{chnr}{mnr}']].values[0]:
+        bp.setSegmentMarker(nm, (0, dr), mnr)
 
 
-def iflistorstr(val):
-    if type(val) in (list, str):
-        val = list_or_sting(val)
-        return val
-    else:
-        return val
+def find_recurcive(df):
+    col_to_tjek = find_channels(df)
+    col_to_tjek.append('time')
+    list_of_lists = []
+    for col in col_to_tjek:
+        list_of_lists += [list_or_sting(x) for x in df[col].values
+                          if type(x) in [list, str]
+                          and len(list_or_sting(x)) == 3]
+    ln = []
+    for li in list_of_lists:
+        ln.append(int(li[-1]))
+    [x for x in ln if x == ln[0]] == ln
+    return ln[0]
+
+
+def find_channels(df):
+    return [ch for ch in df.columns.values if 'ch' in ch]
 
 
 def getvoltvalues(volt, i=None):
     if isinstance(volt, (int, float)):
-        print('hep')
         return (volt, volt)
     elif type(volt) in (list, str):
         volt = list_or_sting(volt)
@@ -50,45 +78,23 @@ def get_time(t, i=None):
         return t
 
 
-def find_channels(df):
-    return [ch for ch in df.columns.values if 'ch' in ch]
+def iflistorstr(val):
+    if type(val) in (list, str):
+        val = list_or_sting(val)
+        return val
+    else:
+        return val
 
 
-def find_recurcive(df):
-    col_to_tjek = find_channels(df)
-    col_to_tjek.append('time')
-    list_of_lists = []
-    for col in col_to_tjek:
-        list_of_lists += [list_or_sting(x) for x in df[col].values
-                          if type(x) in [list, str]
-                          and len(list_or_sting(x)) == 3]
-    ln = []
-    for li in list_of_lists:
-        ln.append(int(li[-1]))
-    [x for x in ln if x == ln[0]] == ln
-    return ln[0]
+def list_or_sting(val):
+    if type(val) == str:
+        return strtolist(val)
+    else:
+        return val
 
 
-def df_to_seq(df):
-    seq = bb.Sequence()
-    nr_elem = find_recurcive(df)
-    chan_list = find_channels(df)
-    id = df.index.values
-    idnr = id.shape[0]
-    for h in range(nr_elem):
-        elem = bb.Element()
-        for ch in chan_list:
-            bp = bb.BluePrint()
-            bp.setSR(1.2e9)
-            for i in range(idnr):
-                nm = df.iloc[i][['name']].values[0]
-                volt = df.iloc[i][[ch]].values[0]
-                volt = getvoltvalues(volt, h)
-                dr = df.iloc[i][['time']].values[0]
-                dr = get_time(dr, h)*1e-6
-                bp.insertSegment(i, ramp, volt, name=nm, dur=dr)
-            chnr = int(ch[2:])
-            elem.addBluePrint(chnr, bp)
-        seq.addElement(h+1, elem)
-    seq.setSR(1.2e9)
-    return seq
+def strtolist(s):
+    values = [float(i) for i in s.split(',')]
+    if len(values) == 3:
+        values[-1] = int(values[-1])
+    return values
