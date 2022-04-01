097fe585ab1f009758d709bf4a6c06bb4fe67adf
RasmusBC59 <rbcmail@gmail.com>
2022-04-01 14:24:43 +0200

    add get_pulse_list

--- sequencebuilder/drawpulseonplot.py
+++ sequencebuilder/drawpulseonplot.py
@@ -1,5 +1,6 @@
-from qcodes.dataset import load_by_run_spec
 import holoviews as hv
+import numpy as np
+from qcodes.dataset import load_by_run_spec
 from holoviews import streams
 
 hv.extension('bokeh')
@@ -12,15 +13,18 @@ class DrawPulseOnPlot():
         self.double_tap = streams.DoubleTap(rename={'x': 'x2', 'y': 'y2'},
                                             transient=True)
         self.taps = []
+        self.xs = []
+        self.ys = []
+        self.ramp = []
 
     def record_taps(self, x, y, x2, y2):
         if None not in [x, y]:
-            self.taps.append((x, y, 1))
+            self.taps.append((x, y, 0))
         elif None not in [x2, y2]:
-            self.taps.append((x2, y2, 2))
+            self.taps.append((x2, y2, 1))
         pointplot = hv.Points(self.taps,
                               vdims='Taps').opts(color='Taps',
-                                                 cmap={1: 'red', 2: 'gray'},
+                                                 cmap={0: 'red', 1: 'gray'},
                                                  size=10,
                                                  tools=['hover'])
         curveplot = hv.Curve([x[:-1] for x in self.taps])
@@ -37,3 +41,10 @@ class DrawPulseOnPlot():
     def start_plot(self):
         self.taps_dmap = hv.DynamicMap(self.record_taps, streams=[self.tap, self.double_tap])
         return self.pulse_from_plot()*self.taps_dmap
+
+    def get_pulse_lists(self):
+        as_Transpost_np_array = np.array(self.taps).T
+        x = as_Transpost_np_array[0]
+        y = as_Transpost_np_array[1]
+        ramp = as_Transpost_np_array[2]
+        return x, y, ramp
