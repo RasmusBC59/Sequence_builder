04f33946b7fc5a58625103bcb52f6ed1abf10ff0
Rasmus B. Christensen <rasmus.christensen@nbi.ku.dk>
2022-04-08 11:08:08 +0200

    remove telemetry

--- sequencebuilder/__init__.py
+++ sequencebuilder/__init__.py
@@ -1,5 +1,4 @@
-from configparser import ConfigParser
-from pathlib import Path
+
 import logging
 
 from ._version import get_versions
@@ -7,18 +6,8 @@ __version__ = get_versions()['version']
 del get_versions
 
 
-import sequencebuilder
-from sequencebuilder.telemetry import start_telemetry
 
 
-CONFIG_PATH = (Path(Path(sequencebuilder.__file__).parent) /
-               'conf' / 'telemetry.ini')
-
-telemetry_config = ConfigParser()
-telemetry_config.read(CONFIG_PATH)
-
-if telemetry_config['Telemetry'].getboolean('enabled'):
-    start_telemetry()
 
 logger = logging.getLogger(__name__)
 logger.info(f'Imported sequencebuilderversion: {__version__}')
